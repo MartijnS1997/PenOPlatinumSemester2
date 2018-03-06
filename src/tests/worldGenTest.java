213be17b98e8790d3e92c7e536e69edf7da0a515
Jonathan Craessaerts <jonathan.craessaerts@student.kuleuven.be>
2018-03-06 12:33:07 +0100

    worldgenerator+path changes

--- src/tests/worldGenTest.java
+++ src/tests/worldGenTest.java
@@ -1,31 +1,41 @@
-//package tests;
-//
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.util.ArrayList;
-//import java.util.Map;
-//import java.util.Set;
-//import java.util.TreeMap;
-//
-//
-//import internal.Vector;
-//import internal.WorldGenerator;
-//
-//public class worldGenTest {
+package tests;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import internal.Testbed.WorldGenerator;
+
+public class worldGenTest {
+
+    private static int nbOfBlocks = 3;
+    private static WorldGenerator wg = new WorldGenerator(nbOfBlocks);
+
+    public static void main(String[] args) {
+        wg.testFunction(wg.allPositionsGenerator());
+    }
+
+
+
+
+
 //
 //	private static int nbOfBlocks = 100000;
 //	private static WorldGenerator wg = new WorldGenerator(nbOfBlocks);
-//	
-//	
+//
+//
 //	public static void main(String[] args) {
 //		ArrayList<Vector> poss = generatePositions(nbOfBlocks);
-//		
+//
 //		for (int i = 1; i <= 3; i++){
 //			ArrayList<ArrayList<Float>> xx = checkPositions(poss, nbOfBlocks,i);
 //		}
 //	}
 //
-//	
+//
 //
 //	public static ArrayList<Vector> generatePositions(int n){
 //		ArrayList<Vector> pos = new ArrayList<Vector>();
@@ -35,14 +45,14 @@
 //		}
 //		return pos;
 //	}
-//	
+//
 //	public static ArrayList<ArrayList<Float>> checkPositions(ArrayList<Vector> positions, int nb, int dir){
 //		ArrayList<Float> stdDev1 = new ArrayList<>();
 //		ArrayList<Float> stdDev2 = new ArrayList<>();
 //		ArrayList<Float> stdDev3 = new ArrayList<>();
-//		
+//
 //		String direct;
-//		
+//
 //		if (dir == 1)
 //			direct = "x";
 //		else if (dir == 2)
@@ -51,9 +61,9 @@
 //			direct = "z";
 //		else
 //			direct = "ERROR";
-//		
+//
 //		for (int i = 0; i < nb; i++){
-//			if (dir == 1){	
+//			if (dir == 1){
 //				if ((positions.get(i).getxValue() <= wg.getStdDevX() + wg.getXMean()) &&
 //						(positions.get(i).getxValue() >= -wg.getStdDevX() + wg.getXMean())){
 //					stdDev1.add(positions.get(i).getxValue());
@@ -67,7 +77,7 @@
 //					stdDev3.add(positions.get(i).getxValue());
 //				}
 //			}
-//			if (dir == 2){	
+//			if (dir == 2){
 //				if ((positions.get(i).getyValue() <= wg.getStdDevY() + wg.getYMean()) &&
 //						(positions.get(i).getyValue() >= -wg.getStdDevY() + wg.getYMean())){
 //					stdDev1.add(positions.get(i).getyValue());
@@ -81,7 +91,7 @@
 //					stdDev3.add(positions.get(i).getyValue());
 //				}
 //			}
-//			if (dir == 3){	
+//			if (dir == 3){
 //				if ((positions.get(i).getzValue() <= wg.getStdDevZ() + wg.getZMean()) &&
 //						(positions.get(i).getzValue() >= -wg.getStdDevZ() + wg.getZMean())){
 //					stdDev1.add(positions.get(i).getzValue());
@@ -98,21 +108,21 @@
 //
 //
 //		}
-//		
+//
 //		float pc1 = (float) stdDev1.size() / nb * 100;
 //		float pc2 = (float) stdDev2.size() / nb * 100;
 //		float pc3 = (float) stdDev3.size() / nb * 100;
-//		
+//
 //		System.out.println(pc1+"% of the "+direct+"-positions have a value within the 1st Gaussian interval");
 //		System.out.println(pc2+"% of the "+direct+"-positions have a value within the 2nd Gaussian interval");
 //		System.out.println(pc3+"% of the "+direct+"-positions have a value within the 3rd Gaussian interval");
-//		
+//
 //		ArrayList<ArrayList<Float>> res = new ArrayList<ArrayList<Float>>(3);
 //		res.add(stdDev1);
 //		res.add(stdDev2);
 //		res.add(stdDev3);
 //		return res;
 //	}
-//	
-// 
-//}
+//
+//
+}
