a60acedcbd284201675afbf663d49b2364a3c1a3
JasperCallaerts <jasper.callaerts@gmail.com>
2018-03-10 15:37:27 +0100

    Airport gefixt

--- src/internal/Testbed/WorldBuilder_v2.java
+++ src/internal/Testbed/WorldBuilder_v2.java
@@ -1,14 +1,11 @@
 package internal.Testbed;
 
-import internal.Autopilot.PathGenerator;
 import internal.Helper.Vector;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
-
-import gui.Cube;
 
 /**
  * Created by Martijn on 26/02/2018.
@@ -29,9 +26,9 @@ public class WorldBuilder_v2 {
         List<Drone> droneList = this.getDroneBuilder_v2().createDrones(droneConfig);
         World world = new WorldGenerator(NB_OF_BLOCKS).createWorld();
         world.addDrones(droneList);
-        Floor floor = new Floor(new Vector());
-        world.addWorldObject(floor);
-        Airport airport = new Airport(new Vector(0, 0.01f, -0));
+//        Floor floor = new Floor(new Vector());
+//        world.addWorldObject(floor);
+        Airport airport = new Airport(new Vector());
         world.addWorldObject(airport);
         return world;
     }
@@ -46,8 +43,8 @@ public class WorldBuilder_v2 {
         droneConfig.put(new Vector(0,20f,0), 0f); //drone at 5m height facing forward
         List<Drone> droneList = this.getDroneBuilder_v2().createDrones(droneConfig);
         world.addDrones(droneList);
-        Floor floor = new Floor(new Vector());
-        world.addWorldObject(floor);
+//        Floor floor = new Floor(new Vector());
+//        world.addWorldObject(floor);
         Airport airport = new Airport(new Vector());
         world.addWorldObject(airport);
         return world;
@@ -63,62 +60,24 @@ public class WorldBuilder_v2 {
         List<Drone> droneList = this.getDroneBuilder_v2().createDrones(droneConfig);
         World world = new World(World.NO_OBJECTIVE, threads);
         world.addDrones(droneList);
-        Floor floor = new Floor(new Vector());
-        world.addWorldObject(floor);
+//        Floor floor = new Floor(new Vector());
+//        world.addWorldObject(floor);
         Airport airport = new Airport(new Vector());
         world.addWorldObject(airport);
         return world;
     }
 
     /**
-     * Creates an empty world with only one drone in it and no cubes (for testing takeoff and tire physics)
+     * Creates an empty world with only one drone in it and no cubes (for testing takeoff and tyre physics)
      * @return a world only containing a drone and a floor
      */
     public World createWorld(){
-    	
-    	
         World world = new World(World.NO_OBJECTIVE);
         world.addWorldObject(this.getDroneBuilder_v2().createTestBounceDrone());
         Floor floor = new Floor(new Vector());
         world.addWorldObject(floor);
-//        Airport airport = new Airport(new Vector());
-//        world.addWorldObject(airport);
-
-        
-        // For pathgenerator testing purposes
-        /*
-        PathGenerator pathGenerator = new PathGenerator();
-        Vector position = new Vector(150f, 10f, 0f);
-        Vector destination = new Vector(0f, 0f, 0f);
-        Vector velocity = new Vector(0f, 0f, -30f);
-        velocity = velocity.rotateAroundYAxis(-(float)Math.PI/8);
-        pathGenerator.generateLandingPath(position, velocity, destination);
-        
-        Vector firstColor = new Vector(80f, 80f, 80f);
-        Block firstBlock = new Block(position);
-        Cube firstCube = new Cube(position.convertToVector3f(), firstColor.convertToVector3f(), true);
-        firstCube.setSize(5f);
-        firstBlock.setAssocatedCube(firstCube);
-        world.addWorldObject(firstBlock);
-        
-        Vector secondPosition = position.vectorSum(velocity);
-        Vector secondColor = new Vector(50f, 50f, 50f);
-        Block secondBlock = new Block(secondPosition);
-        Cube secondCube = new Cube(secondPosition.convertToVector3f(), secondColor.convertToVector3f(), true);
-        secondCube.setSize(5f);
-        secondBlock.setAssocatedCube(secondCube);
-        world.addWorldObject(secondBlock);
-        
-        for (Vector blockPos : pathGenerator.getPath()) {
-        	Vector color = new Vector(10f, 10f, 10f);
-            Block block = new Block(blockPos);
-            Cube cube = new Cube(blockPos.convertToVector3f(), color.convertToVector3f(), true);
-            cube.setSize(2f);
-            block.setAssocatedCube(cube);
-            world.addWorldObject(block);
-        }
-        */
-        // END for pathgenerator testing purposes
+        Airport airport = new Airport(new Vector());
+        world.addWorldObject(airport);
         return world;
     }
 
