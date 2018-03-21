70229d4f9c21eb03c66a48d8602ef1456620d83e
MartijnS1997 <martijn.sauwens@gmail.com>
2018-03-21 20:19:31 +0100

    Tried to make the way point controller work... with no success added stabilizer controller class for stabilizing in between flight phases

--- src/TestbedMain.java
+++ src/TestbedMain.java
@@ -289,8 +289,8 @@ public class TestbedMain implements Runnable{
         //World world =  builder.createWorld(); //builder.createWorld(droneConfig)
         //World world = builder.createFlightTestWorld();
         //World world = builder.createTakeoffWorld();
-        //World world = builder.createTotalFlightWorld();
-        World world  = builder.createLandingWorld();
+        World world = builder.createTotalFlightWorld();
+        //World world  = builder.createLandingWorld();
         this.setWorld(world);
         Drone drone =(Drone)(world.getDroneSet().toArray())[0]; //only one drone is present
         //System.out.println("drone: " + drone);
