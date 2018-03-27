3bcdc7170ddf72f9346419748ac95fd91b055b27
Jasper <j_snoeks@hotmail.be>
2018-03-27 16:44:14 +0200

    tussentijds getoonde versie

--- src/internal/Testbed/Floor.java
+++ src/internal/Testbed/Floor.java
@@ -15,10 +15,11 @@ public class Floor implements WorldObject {
 	private Set<GraphicsObject> floorTiles = new HashSet<>();
 
 	public Floor(Vector position) {
-		int n = 41; // n moet oneven zijn
+		int n = 51; // n moet oneven zijn
         float nx = 20f;
         float nz = 20f;
-		this.position = new Vector(-n*nx/2, 0, -n*nz);
+//		this.position = new Vector(-n*nx/2, 0, -n*nz);
+		this.position = new Vector(-n*nx/2, 0, -n*nz/2);
 		createFloor(n, nx, nz);
 	}
 	
@@ -44,7 +45,7 @@ public class Floor implements WorldObject {
 	
 	public void createFloor(int n, float nx, float nz) {
 		
-        for (int i = 0; i < 2*n*n; i++) {
+        for (int i = 0; i < n*n; i++) {
         	Vector delta = new Vector(nx*(i%n), 0, nz*(i/n));
         	Vector position = delta.vectorSum(getPosition());
             Vector color = new Vector((60.0f+(i%2)*60), 1, 0.6f);
